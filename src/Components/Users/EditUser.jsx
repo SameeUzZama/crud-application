ae4e0251a0d54c85c46c47f3e084e489134c0985
Samee Uz Zama <94825943+SameeUzZama@users.noreply.github.com>
2022-09-01 10:36:43 +0530

    update

--- src/Components/Users/EditUser.jsx
+++ src/Components/Users/EditUser.jsx
@@ -1,102 +1,102 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import { useNavigate, useParams } from "react-router-dom";
 
-const EditUser = () => {
-  let history = useNavigate();
-  const { id } = useParams();
+// const EditUser = () => {
+//   let history = useNavigate();
+//   const { id } = useParams();
 
-  const [user, setUser] = useState({
-    name: "",
-    username: "",
-    email: "",
-    number: "",
-    website: "",
-  });
+//   const [user, setUser] = useState({
+//     name: "",
+//     username: "",
+//     email: "",
+//     number: "",
+//     website: "",
+//   });
 
-  useEffect(() => {
-    LoadUser();
-  }, []);
+//   useEffect(() => {
+//     LoadUser();
+//   }, []);
 
-  const { name, username, email, number, website } = user;
+//   const { name, username, email, number, website } = user;
 
-  const onInputChange = (e) => {
-    setUser({ ...user, [e.target.name]: e.target.value });
-    console.log(e.target.value);
-  };
+//   const onInputChange = (e) => {
+//     setUser({ ...user, [e.target.name]: e.target.value });
+//     console.log(e.target.value);
+//   };
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    await axios.put(`http://localhost:3003/users/${id}`, user);
-    history("/");
-  };
+//   const onSubmit = async (e) => {
+//     e.preventDefault();
+//     await axios.put(`http://localhost:3003/users/${id}`, user);
+//     history("/");
+//   };
 
-  const LoadUser = async () => {
-    const result = await axios.get(`http://localhost:3003/users/${id}`);
-    setUser(result.data);
-    console.log(result);
-  };
-  return (
-    <div className="container">
-      <div className="w-75 mx-auto shadow p-1">
-        <h2 className="text-center mb-4">Edit A User</h2>
-        <form onSubmit={(e) => onSubmit(e)}>
-          <div class="form-group m-2">
-            <input
-              type="text"
-              class="form-control form-control-lg"
-              placeholder="Enter Your Name"
-              name="name"
-              value={name}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
-          <div class="form-group m-2">
-            <input
-              type="text"
-              class="form-control form-control-lg"
-              placeholder="Enter Your Username"
-              name="username"
-              value={username}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
-          <div class="form-group m-2">
-            <input
-              type="email"
-              class="form-control form-control-lg"
-              placeholder="Enter Your E-mail Address"
-              name="email"
-              value={email}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
-          <div class="form-group m-2">
-            <input
-              type="number"
-              class="form-control form-control-lg"
-              placeholder="Enter Your Phone Number"
-              name="number"
-              value={number}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
-          <div class="form-group m-2">
-            <input
-              type="url"
-              class="form-control form-control-lg"
-              placeholder="Enter Your Website Name"
-              name="website"
-              value={website}
-              onChange={(e) => onInputChange(e)}
-            />
-          </div>
-          <button class="btn btn-warning btn-block">Update User</button>
-        </form>
-      </div>
-    </div>
-  );
-};
+//   const LoadUser = async () => {
+//     const result = await axios.get(`http://localhost:3003/users/${id}`);
+//     setUser(result.data);
+//     console.log(result);
+//   };
+//   return (
+//     <div className="container">
+//       <div className="w-75 mx-auto shadow p-1">
+//         <h2 className="text-center mb-4">Edit A User</h2>
+//         <form onSubmit={(e) => onSubmit(e)}>
+//           <div class="form-group m-2">
+//             <input
+//               type="text"
+//               class="form-control form-control-lg"
+//               placeholder="Enter Your Name"
+//               name="name"
+//               value={name}
+//               onChange={(e) => onInputChange(e)}
+//             />
+//           </div>
+//           <div class="form-group m-2">
+//             <input
+//               type="text"
+//               class="form-control form-control-lg"
+//               placeholder="Enter Your Username"
+//               name="username"
+//               value={username}
+//               onChange={(e) => onInputChange(e)}
+//             />
+//           </div>
+//           <div class="form-group m-2">
+//             <input
+//               type="email"
+//               class="form-control form-control-lg"
+//               placeholder="Enter Your E-mail Address"
+//               name="email"
+//               value={email}
+//               onChange={(e) => onInputChange(e)}
+//             />
+//           </div>
+//           <div class="form-group m-2">
+//             <input
+//               type="number"
+//               class="form-control form-control-lg"
+//               placeholder="Enter Your Phone Number"
+//               name="number"
+//               value={number}
+//               onChange={(e) => onInputChange(e)}
+//             />
+//           </div>
+//           <div class="form-group m-2">
+//             <input
+//               type="url"
+//               class="form-control form-control-lg"
+//               placeholder="Enter Your Website Name"
+//               name="website"
+//               value={website}
+//               onChange={(e) => onInputChange(e)}
+//             />
+//           </div>
+//           <button class="btn btn-warning btn-block">Update User</button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default EditUser;
+// export default EditUser;
--- src/Components/Users/ViewUser.jsx
+++ src/Components/Users/ViewUser.jsx
@@ -1,54 +1,54 @@
-import React from "react";
-import axios from "axios";
-import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+// import React from "react";
+// import axios from "axios";
+// import { useState, useEffect } from "react";
+// import { Link, useParams } from "react-router-dom";
 
-const ViewUser = () => {
-  const [user, setUser] = useState({
-    name: "",
-    username: "",
-    email: "",
-    phone: "",
-    website: "",
-  });
+// const ViewUser = () => {
+//   const [user, setUser] = useState({
+//     name: "",
+//     username: "",
+//     email: "",
+//     phone: "",
+//     website: "",
+//   });
 
-  const { id } = useParams();
+//   const { id } = useParams();
 
-  useEffect(() => {
-    loadUser();
-  }, []);
+//   useEffect(() => {
+//     loadUser();
+//   }, []);
 
-  const loadUser = async () => {
-    const res = await axios.get(`http://localhost:3003/users/${id}`);
-    setUser(res.data);
-  };
+//   const loadUser = async () => {
+//     const res = await axios.get(`http://localhost:3003/users/${id}`);
+//     setUser(res.data);
+//   };
 
-  return (
-    <div className="container">
-      <h1 className="display-1">User Id:{id}</h1>
-      <hr />
-      <ul className="list-group m-5">
-        <li className="list-group-item">
-          <b>Name:</b> {user.name}
-        </li>
-        <li className="list-group-item">
-          <b>User Name:</b> {user.username}
-        </li>
-        <li className="list-group-item">
-          <b>Email:</b> {user.email}
-        </li>
-        <li className="list-group-item">
-          <b>Phone:</b> {user.phone}
-        </li>
-        <li className="list-group-item">
-          <b>Website:</b> {user.website}
-        </li>
-      </ul>
-      <Link className="btn btn-primary" to="/">
-        Back To Home
-      </Link>
-    </div>
-  );
-};
+//   return (
+//     <div className="container">
+//       <h1 className="display-1">User Id:{id}</h1>
+//       <hr />
+//       <ul className="list-group m-5">
+//         <li className="list-group-item">
+//           <b>Name:</b> {user.name}
+//         </li>
+//         <li className="list-group-item">
+//           <b>User Name:</b> {user.username}
+//         </li>
+//         <li className="list-group-item">
+//           <b>Email:</b> {user.email}
+//         </li>
+//         <li className="list-group-item">
+//           <b>Phone:</b> {user.phone}
+//         </li>
+//         <li className="list-group-item">
+//           <b>Website:</b> {user.website}
+//         </li>
+//       </ul>
+//       <Link className="btn btn-primary" to="/">
+//         Back To Home
+//       </Link>
+//     </div>
+//   );
+// };
 
-export default ViewUser;
+// export default ViewUser;
